e9ef27a57f1b259fed9bd4bd6482ddaeb823f90a
Jenner LaFave <jenner@jfave.com>
2012-08-17 17:06:50 -0700

    UI and functionality updates

--- idea.rb
+++ idea.rb
@@ -4,26 +4,49 @@ require 'data_mapper'
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/data.db")
 
+helpers do
+    def protected!
+        unless authorized?
+          response['WWW-Authenticate'] = %(Basic realm="ideabox v0.2")
+          throw :halt, [401, "Not authorized\n"]
+        end
+    end
+
+    def authorized?
+        authorized = YAML::load(IO.read(File.join(File.dirname(__FILE__), 'auth.yml')))
+        @auth ||= Rack::Auth::Basic::Request.new(request.env)
+        valid = @auth.provided? && @auth.basic? && @auth.credentials && authorized.include?(@auth.credentials[0])
+        valid && authorized[@auth.credentials[0]] == @auth.credentials[1]
+    end
+end
+
 class Idea
     include DataMapper::Resource
     property :id, Serial
     property :title, String
     property :text, Text
+    property :highdea, Boolean
+    property :complete, Boolean
+    property :completion_time, DateTime
     property :created, DateTime
+    property :karma, Integer, :default => 1
 end
 
 DataMapper.finalize.auto_upgrade!
 
 get '/' do
-    @ideas = Idea.all :order => [ :id.desc ]
+    protected!
+    @ideas = Idea.all :order => [ :karma.desc, :id.desc ]
     @title = "List"
     erb :ideas
 end
 
 post '/' do
+    protected!
     @idea = Idea.create(
         :title => params[:title],
         :text => params[:ideatext],
+        :highdea => (params[:highdea] == "true") || false,
         :created => Time.now
     )
     redirect '/'
@@ -40,24 +63,64 @@ get '/:id' do
 end
 
 post '/:id' do
-    lidea = Idea.get params[:id]
-    if lidea == nil
-        redirect '/'
-    end
-    lidea.update(
+    protected!
+    idea = Idea.get params[:id]
+    return "Idea ##{params[:id]} not found." if not idea
+    idea.update(
         :title => params[:title],
         :text => params[:ideatext],
+        :highdea => (params[:highdea] == "true") || false,
     )
     redirect '/'
 end
 
+get '/:id/upvote' do
+    protected!
+    idea = Idea.get params[:id]
+    redirect '/' if not idea
+    idea.karma += 1
+    idea.save!
+    redirect back
+end
+
+get '/:id/downvote' do
+    protected!
+    idea = Idea.get params[:id]
+    redirect '/' if not idea
+    idea.karma -= 1
+    idea.save!
+    redirect back
+end
+
+get '/:id/complete' do
+    protected!
+    idea = Idea.get params[:id]
+    redirect '/' if not idea
+    idea.complete = true
+    idea.completion_time = Time.now
+    idea.save!
+    redirect back
+end
+
+get '/:id/uncomplete' do
+    protected!
+    idea = Idea.get params[:id]
+    redirect '/' if not idea
+    idea.complete = false
+    idea.completion_time = nil
+    idea.save!
+    redirect back
+end
+
 get '/:id/delete' do
+    protected!
     @idea = Idea.get params[:id]
     @title = "Deleting " + @idea.title
     erb :delete
 end
 
 delete '/:id' do
+    protected!
     idea = Idea.get params[:id]
     idea.destroy || "Could not find an idea with that title."
     redirect '/'
